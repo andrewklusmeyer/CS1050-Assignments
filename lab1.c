53030ab8f80192139cedefa510a12651675e3da2
Andrew Klusmeyer <andrew.klusmeyer@gmail.com>
2022-09-20 16:04:00 -0500

    Lab 1
    Within this lab, I was tasked to create a simple "Hello World!" program.

---
+++ lab1.c
@@ -0,0 +1,10 @@
+#include<stdio.h> // This portion includes a pre-built library of functions
+
+int main(void)//This portion is like the "driver" as it will look for "main" when compiled
+{
+    printf("Hello World!\n"); 
+
+    /*This portion prints everything within the quotation marks 
+     * along with creating a new line using \n */
+
+    }
