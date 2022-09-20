da055394f107849618071e7b5097ee56310155a7
Andrew Klusmeyer <andrew.klusmeyer@gmail.com>
2022-09-20 16:30:50 -0500

    Lab 4
    Within this lab, I was tasked to create a program that spells the word of the integer from all integers 1-99 using a switch. For example, if the integer is equal to 2, the program will print "Two". After completing this lab, I noticed that a much more efficient approach to this task would be through integer division. For example, by dividing the integer by 10, you could determine what value (twenty, thirty, forty, etc.) would be in the tens place and using the remainder (one, two, three) within the ones place.

---
+++ lab4.c
@@ -0,0 +1,423 @@
+/********************************
+ * CMP_SC 1050
+ * FS 2022
+ * Lab 4
+ * Written By Andrew Klusmeyer
+ * September 16th, 2022
+*********************************/
+
+
+
+#include <stdio.h> //Includes library of functions
+
+int main(void) //"Start Button"
+{
+    for(int x=1;x<=99; x++)
+
+        switch (x) //determines which x value is inputed
+
+            {
+
+            case(1): //Checks if x is equal to 1
+            printf("One, "); //If x is equal to 1, prints "One"
+            break; //Breaks out of switch and goes back to for loop
+
+            case(2):
+            printf("Two, ");
+            break;
+
+            case(3):
+            printf("Three, ");
+            break;
+
+            case(4):
+            printf("Four, ");
+            break;
+
+            case(5):
+            printf("Five, ");
+            break;
+
+            case(6):
+            printf("Six, ");
+            break;
+
+            case(7):
+            printf("Seven, ");
+            break;
+
+            case(8):
+            printf("Eight, ");
+            break;
+
+            case(9):
+            printf("Nine, ");
+            break;
+
+            case(10):
+            printf("Ten, ");
+            break;
+
+            case(11):
+            printf("Eleven, ");
+            break;
+
+            case(12):
+            printf("Twelve, ");
+            break;
+
+            case(13):
+            printf("Thirteen, ");
+            break;
+
+            case(14):
+            printf("Fourteen, ");
+            break;
+
+            case(15):
+            printf("Fifteen, ");
+            break;
+
+            case(16):
+            printf("Sixteen, ");
+            break;
+
+            case(17):
+            printf("Seventeen, ");
+            break;
+
+            case(18):
+            printf("Eighteen, ");
+            break;
+
+            case(19):
+            printf("Nineteen, ");
+            break;
+
+            case(20):
+            printf("Twenty, ");
+            break;
+
+            case(21):
+            printf("Twenty-One, ");
+            break;
+
+            case(22):
+            printf("Twenty-Two, ");
+            break;
+
+            case(23):
+            printf("Twenty-Three, ");
+            break;
+
+            case(24):
+            printf("Twenty-Four, ");
+            break;
+
+            case(25):
+            printf("Twenty-Five, ");
+            break;
+
+            case(26):
+            printf("Twenty-Six, ");
+            break;
+
+            case(27):
+            printf("Twenty-Seven, ");
+            break;
+
+            case(28):
+            printf("Twenty-Eight, ");
+            break;
+
+            case(29):
+            printf("Twenty-Nine, ");
+            break;
+
+            case(30):
+            printf("Thirty, ");
+            break;
+
+            case(31):
+            printf("Thirty-One, ");
+            break;
+
+            case(32):
+            printf("Thirty-Two, ");
+            break;
+
+            case(33):
+            printf("Thirty-Three, ");
+            break;
+
+            case(34):
+            printf("Thirty-Four, ");
+            break;
+
+            case(35):
+            printf("Thirty-Five, ");
+            break;
+
+            case(36):
+            printf("Thirty-Six, ");
+            break;
+
+            case(37):
+            printf("Thirty-Seven, ");
+            break;
+
+            case(38):
+            printf("Thirty-Eight, ");
+            break;
+
+            case(39):
+            printf("Thirty-Nine, ");
+            break;
+
+            case(40):
+            printf("Fourty, ");
+            break;
+
+            case(41):
+            printf("Fourty-One, ");
+            break;
+
+            case(42):
+            printf("Fourty-Two, ");
+            break;
+
+            case(43):
+            printf("Fourty-Three, ");
+            break;
+
+            case(44):
+            printf("Fourty-Four, ");
+            break;
+
+            case(45):
+            printf("Fourty-Five, ");
+            break;
+
+            case(46):
+            printf("Fourty-Six, ");
+            break;
+
+            case(47):
+            printf("Fourty-Seven, ");
+            break;
+
+            case(48):
+            printf("Fourty-Eight, ");
+            break;
+
+            case(49):
+            printf("Fourty-Nine, ");
+            break;
+
+            case(50):
+            printf("Fifty, ");
+            break;
+
+            case(51):
+            printf("Fifty-One, ");
+            break;
+
+            case(52):
+            printf("Fifty-Two, ");
+            break;
+
+            case(53):
+            printf("Fifty-Three, ");
+            break;
+
+            case(54):
+            printf("Fifty-Four, ");
+            break;
+
+            case(55):
+            printf("Fifty-Five, ");
+            break;
+
+            case(56):
+            printf("Fifty-Six, ");
+            break;
+
+            case(57):
+            printf("Fifty-Seven, ");
+            break;
+
+            case(58):
+            printf("Fifty-Eight, ");
+            break;
+
+            case(59):
+            printf("Fifty-Nine, ");
+            break;
+
+            case(60):
+            printf("Sixty, ");
+            break;
+
+            case(61):
+            printf("Sixty-One, ");
+            break;
+
+            case(62):
+            printf("Sixty-Two, ");
+            break;
+
+            case(63):
+            printf("Sixty-Three, ");
+            break;
+
+            case(64):
+            printf("Sixty-Four, ");
+            break;
+
+            case(65):
+            printf("Sixty-Five, ");
+            break;
+
+            case(66):
+            printf("Sixty-Six, ");
+            break;
+
+            case(67):
+            printf("Sixty-Seven, ");
+            break;
+
+            case(68):
+            printf("Sixty-Eight, ");
+            break;
+
+            case(69):
+            printf("Sixty-Nine, ");
+            break;
+
+            case(70):
+            printf("Seventy, ");
+            break;
+
+            case(71):
+            printf("Seventy-One, ");
+            break;
+
+            case(72):
+            printf("Seventy-Two, ");
+            break;
+
+            case(73):
+            printf("Seventy-Three, ");
+            break;
+
+            case(74):
+            printf("Seventy-Four, ");
+            break;
+
+            case(75):
+            printf("Seventy-Five, ");
+            break;
+
+            case(76):
+            printf("Seventy-Six, ");
+            break;
+
+            case(77):
+            printf("Seventy-Seven, ");
+            break;
+
+            case(78):
+            printf("Seventy-Eight, ");
+            break;
+
+            case(79):
+            printf("Seventy-Nine, ");
+            break;
+
+            case(80):
+            printf("Eighty, ");
+            break;
+
+            case(81):
+            printf("Eighty-One, ");
+            break;
+
+            case(82):
+            printf("Eighty-Two, ");
+            break;
+
+            case(83):
+            printf("Eighty-Three, ");
+            break;
+
+            case(84):
+            printf("Eighty-Four, ");
+            break;
+
+            case(85):
+            printf("Eighty-Five, ");
+            break;
+
+            case(86):
+            printf("Eighty-Six, ");
+            break;
+
+            case(87):
+            printf("Eighty-Seven, ");
+            break;
+
+            case(88):
+            printf("Eighty-Eight, ");
+            break;
+
+            case(89):
+            printf("Eighty-Nine, ");
+            break;
+
+            case(90):
+            printf("Ninety, ");
+            break;
+
+            case(91):
+            printf("Ninety-One, ");
+            break;
+
+            case(92):
+            printf("Ninety-Two, ");
+            break;
+
+            case(93):
+            printf("Ninety-Three, ");
+            break;
+
+            case(94):
+            printf("Ninety-Four, ");
+            break;
+
+            case(95):
+            printf("Ninety-Five, ");
+            break;
+
+            case(96):
+            printf("Ninety-Six, ");
+            break;
+
+            case(97):
+            printf("Ninety-Seven, ");
+            break;
+
+            case(98):
+            printf("Ninety-Eight, ");
+            break;
+
+            case(99):
+            printf("Ninety-Nine\n");
+            break;
+
+            }
+        
+    
+
+
+    return 0;
+}
